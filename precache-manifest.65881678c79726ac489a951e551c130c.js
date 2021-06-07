6e349181959260b146f47d096916ee002aba6cc1
Lukaszk01 <lukaszkkoziel@gmail.com>
2021-06-07 19:35:03 +0900

    Updates

---
+++ precache-manifest.65881678c79726ac489a951e551c130c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cfb1743799d57bd375ac1e14b90c101",
+    "url": "/react-weather-app/index.html"
+  },
+  {
+    "revision": "36ff2c8113442835f252",
+    "url": "/react-weather-app/static/css/main.d678b180.chunk.css"
+  },
+  {
+    "revision": "18c092061e1ce1aa483c",
+    "url": "/react-weather-app/static/js/2.78c77406.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/react-weather-app/static/js/2.78c77406.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36ff2c8113442835f252",
+    "url": "/react-weather-app/static/js/main.8545e324.chunk.js"
+  },
+  {
+    "revision": "fc82a8ae1147bd102cce",
+    "url": "/react-weather-app/static/js/runtime-main.e5a293cb.js"
+  },
+  {
+    "revision": "dd69931ea7f53e690f621e8209c5a8c5",
+    "url": "/react-weather-app/static/media/e51d976087c3ce5b698c3e56de0bc6d8.dd69931e.jpeg"
+  }
+]);
